6bcdf4693808e6d6f56550370030f49a98a99ddc
Валерий Четков <chetkov.valeriy@gmail.com>
2018-10-20 07:52:07 +0300

    - извлечение массива объектов

--- src/ObjectExtractionService.php
+++ src/ObjectExtractionService.php
@@ -25,11 +25,29 @@ class ObjectExtractionService
     }
 
     /**
-     * @param $object
+     * @param object|array $object
      * @return array
      */
     public function extract($object): array
     {
-        return $this->extractionStrategy->extract($object);
+        if (null === $object) {
+            return [];
+        }
+
+        if ($object instanceof \stdClass) {
+            $object = json_decode(json_encode($object), true);
+        }
+
+        if ($object instanceof \ArrayAccess) {
+            foreach ($object as $key => $value) {
+                $object[$key] = $this->extract($value);
+            }
+        }
+
+        if (is_object($object)) {
+            $object = $this->extractionStrategy->extract($object);
+        }
+
+        return $object;
     }
 }
